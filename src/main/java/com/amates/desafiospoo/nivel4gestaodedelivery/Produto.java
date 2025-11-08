38b9c9adb799ac71a967cce13451749176f21e5e
luanates <lucas.amates@gmail.com>
2025-11-08 17:22:21 -0300

    Commit add desafio nível 4 Gestao de Delivery

---
+++ src/main/java/com/amates/desafiospoo/nivel4gestaodedelivery/Produto.java
@@ -0,0 +1,32 @@
+package com.amates.desafiospoo.nivel4gestaodedelivery;
+
+public class Produto {
+    private String nome;
+    private Double preco;
+
+    public Produto(String nome, Double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%nProduto: %s | R$ %f%n", this.nome, this.preco);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+}
