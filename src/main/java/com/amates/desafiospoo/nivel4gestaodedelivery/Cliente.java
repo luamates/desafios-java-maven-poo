29cb3f122ee65fa6e4358399e061460cf84014f6
luanates <lucas.amates@gmail.com>
2025-11-08 17:25:38 -0300

    Gestao de Delivery class cliente

---
+++ src/main/java/com/amates/desafiospoo/nivel4gestaodedelivery/Cliente.java
@@ -0,0 +1,32 @@
+package com.amates.desafiospoo.nivel4gestaodedelivery;
+
+public class Cliente {
+    private String nome;
+    private String endereco;
+
+    public Cliente(String nome, String endereco) {
+        this.nome = nome;
+        this.endereco = endereco;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%n%nNome: %s | Endereço: %s%n", this.nome, this.endereco);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
